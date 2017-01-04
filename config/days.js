aa4d7d7e2bcf6e3645d2b46064910f7985ac16dc
ElaineWang0624 <congying1112@gmail.com>
2017-01-05 01:05:08 +0800

    days

---
+++ config/days.js
@@ -0,0 +1,27 @@
+var days_config = {
+	view:"treetable",
+	columns:[
+		{
+			id: "class",
+			header: "类别",
+			width: 350
+		},
+		{
+			id:"total",
+			header:"额度",
+			width:100
+		},
+		{
+			id:"used",
+			header:"已用",
+			width:100
+		},
+		{
+			id:"left",
+			header:"余额",
+			width:100
+		}
+	],
+	autoheight:true,
+	autowidth:true
+}
---
+++ data/days.js
@@ -0,0 +1,12 @@
+var days = [
+	{
+		"class": "工作日晚上",
+		"total": 100,
+		"used":4
+	},
+	{
+		"class": "双休",
+		"total": 42,
+		"used": 0
+	}
+];
