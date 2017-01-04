31291b52a1997d3f0a1a1856368c5e51ca1f1ab2
congying1112 <congying1112@gmail.com>
2017-01-04 21:14:43 +0800

    first commit

---
+++ data/tasks.js
@@ -0,0 +1,37 @@
+var tasks = [
+	{
+		"name": "task1",
+		"subtasks": [
+			{
+				"name": "subtask1",
+				"subtasks": [
+					{
+						"name": "subtask1_of_subtask1",
+						"hours_needed": 100,
+						"hours_used": 10
+					},
+					{
+						"name": "subtask2_of_subtask1",
+						"hours_needed": 200,
+						"hours_used": 20
+					}
+				]
+			},
+			{
+				"name": "subtask2",
+				"subtasks": [
+					{
+						"name": "subtask1_of_subtask2",
+						"hours_needed": 300,
+						"hours_used": 30
+					},
+					{
+						"name": "subtask2_of_subtask2",
+						"hours_needed": 400,
+						"hours_used": 40
+					}
+				]
+			}
+		]
+	}
+]
