14daff4af0adad626cc77e19df1bdb302817294a
ElaineWang0624 <congying1112@gmail.com>
2017-01-06 21:38:14 +0800

    sample_data

---
+++ sample_data/events.js
@@ -0,0 +1,30 @@
+events.diaries = [
+	{
+		start: '2017-01-04',
+		className: ['day-good']
+	},
+	{
+		start: '2017-01-05',
+		className: ['day-common']
+	},
+	{
+		start: '2017-01-06',
+		className: ['day-bad']
+	}
+];
+
+events.cc = [
+				{
+					title: 'cc task',
+					start: '2017-01-04T20:00:00',
+					end: '2017-01-04T23:00:00'
+				}
+			];
+
+events.work = [
+				{
+					title: 'work task',
+					start: '2017-01-04T09:00:00',
+					end: '2017-01-04T18:00:00'
+				}
+			];
---
+++ sample_data/tasks.js
@@ -0,0 +1,22 @@
+var tasks = [
+	{
+		"value": "日历工具养成",
+		"open": true,
+		"data": [
+			{
+				"value": "日记工具第一版",
+				"hours_used": 6
+			},
+			{
+				"value": "日历数据init",
+				"hours_used": 3
+			}
+		]
+	},
+	{
+		"value": "task1",
+		"plan": "3天规划6天完成",
+		"open": true,
+		"data": []
+	}
+];
